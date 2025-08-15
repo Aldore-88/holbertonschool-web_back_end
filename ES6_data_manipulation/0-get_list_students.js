15f5342c3d44cfc874ffc15b28370427b4d161f5
Bevan Tan <btan400@gmail.com>
2025-08-15 11:46:32 +1000

    0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+    const array = [];
+    const Guillaume = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+    const James = { id: 2, firstName: 'James', location: 'Columbia' };
+    const Serena = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+    array.push(Guillaume, James, Serena);
+    return array;
+}
