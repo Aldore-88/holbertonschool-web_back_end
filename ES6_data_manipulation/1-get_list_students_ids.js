c757324193d59f02b17057bcd7005ece3c66b5fb
Bevan Tan <btan400@gmail.com>
2025-08-22 11:04:45 +1000

    3-get_ids_sum.js

--- ES6_data_manipulation/1-get_list_students_ids.js
+++
@@ -1,7 +0,0 @@
-export default function getListStudentIds(arrayObjs) {
-    if (!Array.isArray(arrayObjs)) {
-        return [];
-    }
-    const newArray = arrayObjs.map((x) => x.id);
-    return newArray;
-}
