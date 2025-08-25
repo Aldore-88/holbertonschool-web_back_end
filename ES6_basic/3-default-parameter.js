22ff27529e5c68e40a9eedc50f9accc9e1131b4a
Bevan Tan <btan400@gmail.com>
2025-08-25 23:17:28 +1000

    3-default-parameter.js

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,9 @@
+
+// intialise function = input of function => functionality;
+const getSumOfHoods = (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + expansion1989 + expansion2019;
+
+export default getSumOfHoods;
+
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+    return( )
+}
