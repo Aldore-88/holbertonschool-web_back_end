f9604f82364eccae8f67c744b22032cdab19bde0
Bevan Tan <btan400@gmail.com>
2025-08-29 00:46:05 +1000

    (income)

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -3,10 +3,10 @@ import getBudgetObject from './7-getBudgetObject.js';
 export default function getFullBudgetObject(income, gdp, capita) {
   return {
     ...getBudgetObject(income, gdp, capita),
-    getIncomeInDollars: income => {
+    getIncomeInDollars: (income) => {
       return `$${income}`;
     },
-    getIncomeInEuros: income => {
+    getIncomeInEuros: (income) => {
       return `${income} euros`;
     },
   };
