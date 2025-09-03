3d9067a08b866d70f17b1d4f9bfd7917475f6b94
Bevan Tan <btan400@gmail.com>
2025-09-03 11:52:43 +1000

    11-createEmployeeObject.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,10 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const idx in array) {
-    array[idx] = appendString + array[idx];
+  for (const idx of array) {
+    let value = array[idx];
+    array[idx] = array + appendString;
+    console.log("idx: " + idx);
+    console.log("value: " + value);
+    console.log("array[idx]: " + array[idx]);
   }
 
   return array;
---
+++ ES6_basic/11-createEmployeeObject.js
@@ -0,0 +1,11 @@
+export default function createEmployeesObject(departmentName, employees) {
+    // creating list of employees
+    for (let employee of employees){
+        //concat employee list here
+
+    }
+
+    return {
+        `${departmentName}: ${employees}`
+    }
+}
