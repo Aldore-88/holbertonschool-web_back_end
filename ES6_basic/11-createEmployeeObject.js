d33b2c2c23da0bac98f773b52f3125b5a6c34b89
Bevan Tan <btan400@gmail.com>
2025-09-04 23:15:13 +1000

    11-createEmployeeObject.js

--- ES6_basic/11-createEmployeeObject.js
+++ ES6_basic/11-createEmployeeObject.js
@@ -1,11 +1,6 @@
 export default function createEmployeesObject(departmentName, employees) {
     // creating list of employees
-    for (let employee of employees){
-        //concat employee list here
-
-    }
-
     return {
-        `${departmentName}: ${employees}`
-    }
+        [`${departmentName}`]: employees,
+    };
 }
