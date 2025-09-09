d17c0b882af9eece367075669976c338b5e16d08
Bevan Tan <btan400@gmail.com>
2025-09-09 17:03:23 +1000

    2-hbtn_course.js

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,39 @@
 export default class HolbertonCourse{
     constructor(name, length, students){
-        if name === somethings
-
-        if length is 
+        this.name - name;
+        this.length = length;
+        this.students = students;
     }
+
+    //name getter setter
+    get name(){
+        return this._name;
+    }
+
+    set name(NewName){
+        if (typeof NewName === 'string'){
+            this._name = NewName;
+        }
+        else {
+            throw new TypeError('Name must be a string');
+        }
+    }
+
+    //length getter setter
+    get length(){
+        return this._length;
+    }
+
+    set length(NewLength){
+        if (typeof NewLength === 'number'){
+            this._length = NewLength;
+        }
+        else {
+            throw new TypeError('Length must be a number')
+        }
+    }
+
+    //student getter setter
+    
 
 }
