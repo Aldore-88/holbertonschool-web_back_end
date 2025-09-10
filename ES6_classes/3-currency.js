6967b6dcce7162b89b99e0f5847e66fa9424b738
Bevan Tan <btan400@gmail.com>
2025-09-10 13:40:10 +1000

    3-currency.js

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency{
+    constructor(symbol, currencyName){
+        this.symbol = symbol;
+        this.currencyName = currencyName;
+    }
+    
+    get symbol(){
+        return this._symbol;
+    }
+
+    set symbol(newSymbol){
+        this._symbol = newSymbol;
+    }
+
+    get currencyName(){
+        return this._currencyName;
+    }
+
+    set currencyName(name){
+        this._currencyName = name;
+    }
+
+    displayfullcurrency(){
+        return (`${this._symbol} (${this._currencyName})`);
+    }
+}
