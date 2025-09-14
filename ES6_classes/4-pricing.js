d6ba4abe1e0f4e6c35d4a3b7e48598fe661224d6
Bevan Tan <btan400@gmail.com>
2025-09-14 23:51:40 +1000

    4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,10 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    // Create objs
+    this._amount = amount;
+    this._currency = currency;
+  }
+  
+}
